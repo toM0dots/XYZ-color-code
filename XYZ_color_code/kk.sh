d5de67dbe4dd828e18e86996b976016f91fa8933
Haoqi Tang <tang1014@ahl03.agate.msi.umn.edu>
2025-07-07 12:14:39 -0500

    kk fixed

--- XYZ_color_code/kk.sh
+++ XYZ_color_code/kk.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --array=6
-#SBATCH --job-name="UH-N18"
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=125
-#SBATCH --mem=20gb
-#SBATCH --time=5:00:00
-#SBATCH -p msismall
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=tang1014@umn.edu
-#SBATCH --output="slurm_out/%A-%a/output.txt"
+# SBATCH --array=6
+# SBATCH --job-name="UH-N18"
+# SBATCH --ntasks=1
+# SBATCH --cpus-per-task=125
+# SBATCH --mem=20gb
+# SBATCH --time=5:00:00
+# SBATCH -p msismall
+# SBATCH --mail-type=ALL
+# SBATCH --mail-user=tang1014@umn.edu
+# SBATCH --output="slurm_out/%A-%a/output.txt"
 
 #load and activate local python environment
 module load conda
 source activate decoder_env
-# cd ~/XYZ_color_code
+cd ~/XYZ-color-code/XYZ_color_code
 
 WORKDIR=slurm_out/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}
 
@@ -29,10 +29,10 @@ WORKDIR=slurm_out/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}
 # step=${step_arr[${SLURM_ARRAY_TASK_ID}]}
 # num_samples=${num_samples_arr[${SLURM_ARRAY_TASK_ID}]}
 ######------------------------------------------------------
-seeds= $(seq 0 2 1999)
+seeds=$(seq 0 2 1999)
 error_rate=0.0002
-H= 12
-L= 9
+H=12
+L=9
 #######################################################################
 
 python3 benchmark.py --H ${H} --L ${L} --error_rate ${error_rate} --seeds ${seeds}
